8f8e84625d1402af6fad566ef785debe62d1f30d
Martin Lipták <martin.liptak108@outlook.com>
2022-08-31 17:57:39 +0200

    Added job to remove unused carts, added javadoc to service layer interfaces

--- src/main/java/sk/martinliptak/ita/ItaApplication.java
+++ src/main/java/sk/martinliptak/ita/ItaApplication.java
@@ -2,8 +2,10 @@ package sk.martinliptak.ita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ItaApplication {
 
     public static void main(String[] args) {
--- src/main/java/sk/martinliptak/ita/service/CartService.java
+++ src/main/java/sk/martinliptak/ita/service/CartService.java
@@ -2,8 +2,34 @@ package sk.martinliptak.ita.service;
 
 import sk.martinliptak.ita.model.CartDto;
 
+/**
+ * A service for manipulating carts
+ * @see sk.martinliptak.ita.domain.Cart
+ *
+ */
 public interface CartService {
+    /**
+     * Retrieves specific cart
+     * @param id of cart
+     * @return {@link CartDto}
+     * @throws sk.martinliptak.ita.exception.CartNotFoundException if the cart doesn't not exist
+     */
     CartDto findCart(Long id);
+
+    /**
+     * Creates new cart for given product
+     * @param productId
+     * @return {@link CartDto}
+     * @throws sk.martinliptak.ita.exception.ProductNotFoundException if the product doesn't exist
+     */
     CartDto createCart(Long productId);
+
+    /**
+     * Adds product to cart
+     * @param id of cart
+     * @param productId
+     * @return {@link  CartDto}
+     * @throws sk.martinliptak.ita.exception.CartNotFoundException if the cart doesn't exist
+     */
     CartDto addToCart(Long id,Long productId);
 }
