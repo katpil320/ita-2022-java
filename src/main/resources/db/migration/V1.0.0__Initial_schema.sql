97bdf11c4c2177d3e7042ef4042af997d80e8c6d
Martin Lipták <martin.liptak108@outlook.com>
2022-09-01 12:04:53 +0200

    updated flyway schema, fixed tests by providing aws credentials in app properties

--- src/main/resources/db/migration/V1.0.0__Initial_schema.sql
+++ src/main/resources/db/migration/V1.0.0__Initial_schema.sql
@@ -1,4 +1,3 @@
-create sequence hibernate_sequence start with 1000 increment by 1;
 create table author
 (
     id          int8 not null,
@@ -47,27 +46,23 @@ create table genre
 );
 create table product
 (
-    id          int8 not null,
-    created_at  timestamp(9),
-    modified_at timestamp(9),
-    description varchar(512),
-    image       varchar(255),
-    name        varchar(255),
-    price       int8,
-    stock       int8,
-    author_id   int8,
-    genre_id    int8,
+    id                int8 not null,
+    created_at        timestamp(9),
+    modified_at       timestamp(9),
+    description       varchar(512),
+    image             varchar(255),
+    name              varchar(255),
+    preview_file_name varchar(255),
+    price             int8,
+    stock             int8,
+    author_id         int8,
+    genre_id          int8,
     primary key (id)
 );
-alter table if exists cart_products
-    add constraint fk_product_id foreign key (fk_product) references product;
-alter table if exists cart_products
-    add constraint fk_cart_id foreign key (fk_cart) references cart;
-alter table if exists eshop_order_products
-    add constraint fk_eshop_order_product_id foreign key (fk_product) references product;
-alter table if exists eshop_order_products
-    add constraint fk_eshop_order_id foreign key (fk_eshop_order) references eshop_order;
-alter table if exists product
-    add constraint fk_product_author_id foreign key (author_id) references author;
-alter table if exists product
-    add constraint fk_product_genre_id foreign key (genre_id) references genre;
+create sequence hibernate_sequence start with 1000 increment by 1;
+alter table if exists cart_products add constraint FK3ajor63f7qykde5i7x0nmdkj0 foreign key (fk_product) references product;
+alter table if exists cart_products add constraint FKbrui6s13kpx85jexpigese280 foreign key (fk_cart) references cart;
+alter table if exists eshop_order_products add constraint FKadkq97m9xjv24k4554sorsuin foreign key (fk_product) references product;
+alter table if exists eshop_order_products add constraint FK6npdvxhdrkj4tn7sooy9xegu6 foreign key (fk_eshop_order) references eshop_order;
+alter table if exists product add constraint FKayq8bdn719whg00c0wor7skjp foreign key (author_id) references author;
+alter table if exists product add constraint FKlgp6x71g08sy3a84mur67uuht foreign key (genre_id) references genre;
